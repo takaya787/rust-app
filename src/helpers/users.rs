afbfac03d6ddff82149cf67612bd15226160d3a6
takaya787 <takaya318318@gmail.com>
2021-12-12 20:54:42 +0900

    refactor: NewUserにcreate関数を設定

--- src/helpers/users.rs
+++ src/helpers/users.rs
@@ -31,24 +31,34 @@ pub fn get_all_users(conn: &PgConnection) -> QueryResult<Vec<User>> {
 }
 
 // Post /users
+impl NewUser {
+  fn create(name: &str, email: String, password_digest: String) -> NewUser {
+    NewUser {
+      name: Some(String::from(name)),
+      email: Some(email),
+      created_at: Utc::now().naive_utc(),
+      updated_at: Utc::now().naive_utc(),
+      password_digest: Some(password_digest),
+      admin: false,
+      activation_digest: None,
+      activated: Some(true),
+      activated_at: None,
+      reset_digest: None,
+      reset_sent_at: None,
+    }
+  }
+}
+
 pub fn create_user<'a>(conn: &PgConnection, userform: &UserForm) -> QueryResult<User> {
   use schema::users;
 
-  let password_hash = hash(userform.password, DEFAULT_COST).expect("Error hashing password");
+  let password_digest = hash(userform.password, DEFAULT_COST).expect("Error hashing password");
 
-  let new_user = NewUser {
-    name: Some(String::from(userform.name)),
-    email: Some(userform.email.to_lowercase()),
-    created_at: Utc::now().naive_utc(),
-    updated_at: Utc::now().naive_utc(),
-    password_digest: Some(String::from(password_hash)),
-    admin: false,
-    activation_digest: None,
-    activated: Some(true),
-    activated_at: None,
-    reset_digest: None,
-    reset_sent_at: None,
-  };
+  let new_user = NewUser::create(
+    userform.name,
+    userform.email.to_lowercase(),
+    String::from(password_digest),
+  );
 
   diesel::insert_into(users::table)
     .values(&new_user)
